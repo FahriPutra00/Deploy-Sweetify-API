b5936632ef22e2da58dd3eff66443c7962f426cf
Fahri Putra <fahriputra000@gmail.com>
2024-06-03 21:31:10 +0700

    update test.py

--- test/test.py
+++ test/test.py
@@ -3,8 +3,13 @@ import requests
 resp = requests.post("http://localhost:5000/", files={'file': open('sample.JPG', 'rb')})
 
 result = resp.json()
+
 print(result)
 print(result.get("Grade"))
 print(result.get("Gula/100ml(g)"))
 print(result.get("Gula/Sajian(g)"))
 print(result.get("Product"))
+
+ml=int(200)#input user volume air
+countsug = float(result.get("Gula/100ml(g)"))*(ml/100)
+print(countsug)
